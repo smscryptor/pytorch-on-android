fa0bf0551e18e48dd0d5667de94cfa4d358f95c9
PHOUM LIB <phoumlib@hotmail.com>
2020-02-28 13:05:09 +0100

    Update setuppytorch.sh

--- setuppytorch.sh
+++ setuppytorch.sh
@@ -6,7 +6,7 @@ pacman -S linux-headers
 pacman -S python-pip
 git clone http://github.com/pytorch/pytorch
 cd pytorch
-git checkout v1.4.0
+git checkout v1.3.1
 git submodule update --init --recursive
 pip install -U setuptools
 pip install -r requirements.txt
